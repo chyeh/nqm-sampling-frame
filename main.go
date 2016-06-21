b32095a952ba6981f85a939c5b54a4b7e13480f2
chyeh <chyeh@cepave.com>
2016-06-21 17:31:59 +0800

    [OWL-505] Add command flags `-v` & `--version`

--- main.go
+++ main.go
@@ -16,11 +16,18 @@ func init() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	pflag.CommandLine.StringP("config", "c", "cfg.json", "configuration file")
 	viper.BindPFlag("config", pflag.Lookup("config"))
+	pflag.CommandLine.BoolP("version", "v", false, "show version")
+	viper.BindPFlag("version", pflag.Lookup("version"))
 }
 
 func main() {
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.Parse()
+
+	if viper.GetBool("version") {
+		fmt.Println(VERSION)
+		os.Exit(0)
+	}
 
 	loadConfigFile()
 
