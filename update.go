c3935c3749f6a5b04ecdef8f6e8a3d007343d416
chyeh <chyeh@cepave.com>
2016-06-21 17:49:47 +0800

    [OWL-505] Reset every single rows before updating
    Set `tg_available` & `tg_status` to false for all of the rows before
    updating.

--- update.go
+++ update.go
@@ -6,11 +6,19 @@ import (
 	"time"
 )
 
+const (
+	resetTargetAvailableSQL = "UPDATE nqm_target SET tg_available=false;"
+	resetTargetStatusSQL    = "UPDATE nqm_target SET tg_status=false;"
+)
+
 func update(interval time.Duration) {
 	ticker := time.NewTicker(time.Minute * interval)
 
 	for {
 		go func() {
+			if _, err := DB.Exec(resetTargetAvailableSQL); err != nil {
+				log.Println(err)
+			}
 			sql, err := ioutil.ReadFile("var/sql/updateNqmTargetAvailable.sql")
 			if err != nil {
 				log.Println(err)
@@ -19,6 +27,9 @@ func update(interval time.Duration) {
 				log.Println(err)
 			}
 
+			if _, err := DB.Exec(resetTargetStatusSQL); err != nil {
+				log.Println(err)
+			}
 			sql, err = ioutil.ReadFile("var/sql/updateNqmTargetStatus.sql")
 			if err != nil {
 				log.Println(err)
